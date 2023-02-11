1ad7b3338382686377c06bb76c8a85d12a3359c3
Ainur Nugumanov <chelovekebola@mail.ru>
2023-02-12 00:45:01 +0500

    Scene update

--- Assets/Source/Runtime/View/Gun/Bullets/BulletTransformView.cs
+++ Assets/Source/Runtime/View/Gun/Bullets/BulletTransformView.cs
@@ -8,6 +8,12 @@ namespace Tanks.View.Gun
 	public class BulletTransformView : MonoBehaviour
 	{
 		private IBullet _bullet;
+		private Rigidbody2D _rigidbody;
+
+		private void Awake()
+		{
+			_rigidbody= GetComponent<Rigidbody2D>();
+		}
 
 		public void Init(IBullet bullet)
 		{
@@ -20,6 +26,7 @@ namespace Tanks.View.Gun
 			{
 				_bullet.Attack(healthTransformView.Health);
 			}
+
 			Destroy(gameObject);
 		}
 	}
